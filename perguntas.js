d628c97927348a0365f9378296fbc629bc42a4a2
juliajmpintanelli <00001105937926SP@al.educacao.sp.gov.br>
2024-11-29 23:17:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Religião',
+    'Onde Nossa Senhora de Fátima apareceu?',
+    'Na cidade de Fátima, em Portugal.'
+)
+
+criaCartao(
+    'Religião',
+    'Em que ano aconteceram as aparições?',
+    'Em 1917.'
+)
+
+criaCartao(
+    'Religião',
+    'Quantas crianças testemunharam as aparições?',
+    'Três crianças.'
+)
+
+criaCartao(
+    'Religião',
+    'Quais os nomes das crianças que viram Nossa Senhora de Fátima?',
+    'Lúcia, Jacinta e Francisco.'
+)
+
+criaCartao(
+    'Religião',
+    'Quantas vezes Nossa Senhora apareceu às crianças?',
+    'Seis vezes.'
+)
+
+criaCartao(
+    'Religião',
+    'Qual a principal mensagem de Nossa Senhora de Fátima?',
+    'Conversão, oração e penitência.'
+)
